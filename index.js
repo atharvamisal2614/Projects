6f10ea1ab3c71327f55168767f05b42e1d2210a4
atharvamisal2614 <103751527+atharvamisal2614@users.noreply.github.com>
2022-12-23 22:18:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+// var randomNumber1=Math.floor(Math.random()*6)+1;
+
+// var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+// var randomImageSource ="images/" + randomDiceImage;
+
+// var image1= document.querySelectorAll("img")[0];
+
+// image1.setAttribute("src", randomImageSource);
+
+
+// var randomNumber2=Math.floor(Math.random()*6)+1;
+
+// var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+// document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+// if(randomNumber1>randomNumber2){
+//     document.querySelector("h1").innerHTML="🚩player 1 Wins!";
+// }
+// else if(randomNumber2>randomNumber1){
+//     document.querySelector("h1").innerHTML="player 2 Wins! 🚩";
+// }
+// else{
+//     document.querySelector("h1").innerHTML="🙂 Maths Draw!.."
+// }
+
+
+//Dice 1
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+
+var query = document.querySelectorAll("img")[0];
+
+query.setAttribute("src",randomDiceImage1);
+
+
+//Dice 2
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+var query = document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML=("🚩player 1 Wins!");
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML=("player 2 Wins! 🚩");
+}
+else{
+    document.querySelector("h1").innerHTML=("🙂 Maths Draw!..");
+}
+
+
+
+
+
+
+
+
+
+
